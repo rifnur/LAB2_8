d2604e2f60e9ebdc3f469e7de80e38e05bad409b
rifnur <rifnur69v@mail.ru>
2020-09-17 17:02:52 +0300

    Уровень 2 Урок 8

---
+++ src/GeekbrainsList.java
@@ -0,0 +1,8 @@
+public interface GeekbrainsList {
+    void addFirst(String o);
+    void addLast(String o);
+    void addByIndex(String o, int index);
+    void removeFirst();
+    void removeLast();
+    void removeIndex(String key);
+}
